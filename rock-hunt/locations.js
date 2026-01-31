70c76d0046e56325828f9bafc6bbd2f35e898b3e
Crew2u <crew@challengeaccepted.quest>
2026-01-31 18:41:33 +1000

    Create locations.js

---
+++ rock-hunt/locations.js
@@ -0,0 +1,19 @@
+// List of prospecting zones
+// Add as many as you like; easy to change coordinates
+
+const PROSPECTING_LOCATIONS = [
+  {
+    id: "test_zone",
+    name: "Test Zone",
+    lat: -26.0000,   // <-- change this to your current location for testing
+    lng: 152.0000,   // <-- change this to your current location for testing
+    radius: 50       // meters to trigger compass/detector
+  },
+  {
+    id: "quartz_ridge",
+    name: "Quartz Ridge",
+    lat: -26.3605,
+    lng: 152.8417,
+    radius: 50
+  }
+];
